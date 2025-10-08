8df73a675e11e55a9a0e3fbe0419718941943562
Flávio Domingos Gomes da Silva <161535510+flaviodgdas@users.noreply.github.com>
2025-10-08 11:09:15 -0300

    whatsapp

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById('form-contato').addEventListener('submit', function(e) {
   e.preventDefault();
   alert('Mensagem enviada com sucesso!');
 });
+// Exemplo: você pode adicionar lógica para mostrar o botão após alguns segundos
+setTimeout(() => {
+  document.getElementById('whatsappBtn').style.display = 'flex';
+}, 1000);
+
